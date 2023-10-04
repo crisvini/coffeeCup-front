9751b5c27ae3cefd0626b4c88218f0f3d1e41577
crisvini <crisvini.leoncini@outlook.com>
2023-10-04 15:33:47 -0300

    login

--- src/routes/SignIn.jsx
+++ src/routes/SignIn.jsx
@@ -2,8 +2,31 @@ import PageTitle from "../components/PageTitle"
 import Logo from '../assets/logo.png'
 import PrimaryBanner from '../assets/primary-banner.svg'
 import { Link } from "react-router-dom"
+import { useState, useEffect } from "react"
 
 const Login = () => {
+
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+
+        const login = {
+            email,
+            password
+        }
+
+        const response = await fetch('http://localhost/api/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(login),
+        })
+        const data = await response.text()
+    }
+
     return (
         <div className="background-quaternary vh-100">
             <PageTitle title='Sign in' />
@@ -17,23 +40,23 @@ const Login = () => {
                             <div className="col-11 col-lg-8 mx-auto text-center fs-4 color-primary mt-3">
                                 <span>coffee cup</span>
                             </div>
-                            <div className="col-11 col-lg-8 mx-auto mt-3">
-                                <input type="email" className="form-control background-secondary color-quaternary" id="email"
-                                    placeholder="E-mail"></input>
-                            </div>
-                            <div className="col-11 col-lg-8 mx-auto mt-3">
-                                <input type="password" className="form-control background-secondary color-quaternary" id="password"
-                                    placeholder="Password"></input>
-                            </div>
-                            <div className="col-11 col-lg-8 mx-auto mt-3">
-                                <button className="btn primary-logo-button-color w-100"
-                                >Sign in</button>
-                            </div>
+                            <form onSubmit={handleSubmit}>
+                                <div className="col-11 col-lg-8 mx-auto mt-3">
+                                    <input type="email" className="form-control background-secondary color-quaternary" id="email"
+                                        placeholder="E-mail" value={email} onChange={(e) => setEmail(e.target.value)}></input>
+                                </div>
+                                <div className="col-11 col-lg-8 mx-auto mt-3">
+                                    <input type="password" className="form-control background-secondary color-quaternary" id="password"
+                                        placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)}></input>
+                                </div>
+                                <div className="col-11 col-lg-8 mx-auto mt-3">
+                                    <input type="submit" className="btn primary-logo-button-color w-100" value="Sign in" />
+                                    {/* <button className="btn primary-logo-button-color w-100">Sign in</button> */}
+                                </div>
+                            </form>
                             <div className="col-11 col-lg-8 mx-auto text-center mt-3">
                                 <Link to="/signup" className="logo-link-color">Don't have an
                                     account? Sign up</Link>
-                                {/* <span className="logo-link-color">Don't have an
-                                    account? Sign up</span> */}
                             </div>
                         </div>
                     </div>
