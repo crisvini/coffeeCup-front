2c231fd73489f3e2c9e76956b287b8132a19db71
crisvini <crisvini.leoncini@outlook.com>
2023-10-26 11:45:01 -0300

    loading

--- src/routes/Discussion.jsx
+++ src/routes/Discussion.jsx
@@ -56,59 +56,65 @@ const Discussion = () => {
     return (
         <div className="background-quaternary">
             <PageTitle title={'Discussion ' + discussion.id} />
-            <Header />
+            {discussion.user_id &&
+                <Header />
+            }
 
-            <main className="container h-100 mt-1">
-                <Back route="/home" />
-                <div className="row background-secondary rounded-lg-3 py-2 px-2 mt-2">
-                    <div className="col-12 px-lg-0 mb-1">
-                        <div className="row">
-                            <div className="col-10 col-lg-8">
-                                <span className="color-primary fs-8">
-                                    <Link to={'/profile/' + discussion.user_id} className="fw-bold primary-link-color">{discussion.user ? formatEmail(discussion.user.email) : "Loading"}</Link>
-                                    &nbsp;published a discussion
-                                </span>
+            {discussion.user_id &&
+                <main className="container h-100 mt-1">
+                    <Back route="/home" />
+
+                    <div className="row background-secondary rounded-lg-3 py-2 px-2 mt-2">
+                        <div className="col-12 px-lg-0 mb-1">
+                            <div className="row">
+                                <div className="col-10 col-lg-8">
+                                    <span className="color-primary fs-8">
+                                        <Link to={'/profile/' + discussion.user_id} className="fw-bold primary-link-color">{discussion.user ? formatEmail(discussion.user.email) : "Loading"}</Link>
+                                        &nbsp;published a discussion
+                                    </span>
+                                </div>
+                                {
+                                    !verifyUser({ userId: discussion.user_id }) ?
+                                        <div className="col-2 col-lg-1 text-lg-end ms-lg-auto">
+                                            <button className="btn btn-sm quaternary-logo-button-color"><i className="bi bi-hand-thumbs-up"></i></button>
+                                        </div>
+                                        :
+                                        <div className="col-2 col-lg-1 text-lg-end ms-lg-auto">
+                                            <button className="btn btn-sm btn-danger" onClick={() => handleDeleteDiscussionButton({ id: data.id })}><i className="bi bi-trash"></i></button>
+                                        </div>
+                                }
                             </div>
-                            {
-                                !verifyUser({ userId: discussion.user_id }) ?
-                                    <div className="col-2 col-lg-1 text-lg-end ms-lg-auto">
-                                        <button className="btn btn-sm quaternary-logo-button-color"><i className="bi bi-hand-thumbs-up"></i></button>
-                                    </div>
-                                    :
-                                    <div className="col-2 col-lg-1 text-lg-end ms-lg-auto">
-                                        <button className="btn btn-sm btn-danger" onClick={() => handleDeleteDiscussionButton({ id: data.id })}><i className="bi bi-trash"></i></button>
-                                    </div>
-                            }
+                        </div>
+                        <div className="col-12 px-lg-0 mb-1">
+                            <h1 className="additional-logo-color">{discussion.title}</h1>
+                        </div>
+                        <div className="col-12 px-lg-0 mb-1">
+                            <span className="color-tertiary fs-6">{discussion.text}</span>
+                        </div>
+                        <div className="col-12 px-lg-0 mb-1">
+                            <span className="color-quaternary fs-10">{formatDate(discussion.created_at)}</span>
                         </div>
                     </div>
-                    <div className="col-12 px-lg-0 mb-1">
-                        <h1 className="additional-logo-color">{discussion.title}</h1>
-                    </div>
-                    <div className="col-12 px-lg-0 mb-1">
-                        <span className="color-tertiary fs-6">{discussion.text}</span>
-                    </div>
-                    <div className="col-12 px-lg-0 mb-1">
-                        <span className="color-quaternary fs-10">{formatDate(discussion.created_at)}</span>
-                    </div>
-                </div>
 
-                <form className="row background-secondary rounded-lg-3 py-2 px-2 mt-3" onSubmit={handleDiscussionAnswer}>
-                    <div className="col-12 px-lg-0">
-                        <textarea className="form-control rounded-3 background-secondary color-quaternary mb-2"
-                            placeholder="Write a answer to this discussion" value={answerText} required onChange={(e) => setAnswerText(e.target.value)}></textarea>
-                    </div>
-                    <div className="ms-auto col-12 col-lg-2 px-lg-0">
-                        <input className="form-control btn btn-sm primary-logo-button-color" type="submit" value='Send' />
-                    </div>
-                </form>
+                    <form className="row background-secondary rounded-lg-3 py-2 px-2 mt-3" onSubmit={handleDiscussionAnswer}>
+                        <div className="col-12 px-lg-0">
+                            <textarea className="form-control rounded-3 background-secondary color-quaternary mb-2"
+                                placeholder="Write a answer to this discussion" value={answerText} required onChange={(e) => setAnswerText(e.target.value)}></textarea>
+                        </div>
+                        <div className="ms-auto col-12 col-lg-2 px-lg-0">
+                            <input className="form-control btn btn-sm primary-logo-button-color" type="submit" value='Send' />
+                        </div>
+                    </form>
 
-                {answers.map((item, index) => (
-                    <Answer key={index} data={item} />
-                ))}
+                    {answers.map((item, index) => (
+                        <Answer key={index} data={item} />
+                    ))}
 
 
-                <br />
-            </main>
+                    <br />
+                </main>
+            }
+
             <LoadingOverlay loading={loading} />
         </div >
     )
--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -105,42 +105,47 @@ const Home = () => {
     return (
         <div className="background-quaternary">
             <PageTitle title='Home' />
-            <Header />
+            {totalDiscussions &&
+                <Header />
+            }
 
-            <main className="container h-100 mt-3">
-                <form className="row background-secondary rounded-lg-3 py-2 px-2" onSubmit={handleDiscussionSubmit}>
-                    <div className="col-12 px-lg-0 mb-3">
-                        <span className="color-primary ps-lg-0 mb-2">Why not start a new discussion?</span>
+            {totalDiscussions &&
+                <main className="container h-100 mt-3">
+                    <form className="row background-secondary rounded-lg-3 py-2 px-2" onSubmit={handleDiscussionSubmit}>
+                        <div className="col-12 px-lg-0 mb-3">
+                            <span className="color-primary ps-lg-0 mb-2">Why not start a new discussion?</span>
+                        </div>
+                        <div className="col-12 px-lg-0">
+                            <input required type="text" className="form-control background-secondary color-quaternary mb-2" id="title"
+                                placeholder="Title" value={title} onChange={(e) => setTitle(e.target.value)}></input>
+                            <textarea className="form-control rounded-3 background-secondary color-quaternary mb-2"
+                                placeholder="Body" value={body} required onChange={(e) => setBody(e.target.value)}></textarea>
+                        </div>
+                        <div className="ms-auto col-12 col-lg-2 px-lg-0">
+                            <input className="form-control btn btn-sm primary-logo-button-color" type="submit" value='Send' />
+                        </div>
+                    </form>
+
+                    <div className="row mt-4 mt-lg-5">
+                        <div className="col-12 col-lg-3 px-lg-0 ms-auto">
+                            <select className="form-select-sm w-100 rounded-3 background-secondary color-quaternary mb-2" onChange={handleDiscussionsFilter} defaultValue={discussionsFilter}>
+                                <option value="1">All discussions</option>
+                                <option value="2">Followed users</option>
+                                <option value="3">My discussions</option>
+                            </select>
+                        </div>
                     </div>
-                    <div className="col-12 px-lg-0">
-                        <input required type="text" className="form-control background-secondary color-quaternary mb-2" id="title"
-                            placeholder="Title" value={title} onChange={(e) => setTitle(e.target.value)}></input>
-                        <textarea className="form-control rounded-3 background-secondary color-quaternary mb-2"
-                            placeholder="Body" value={body} required onChange={(e) => setBody(e.target.value)}></textarea>
-                    </div>
-                    <div className="ms-auto col-12 col-lg-2 px-lg-0">
-                        <input className="form-control btn btn-sm primary-logo-button-color" type="submit" value='Send' />
-                    </div>
-                </form>
 
-                <div className="row mt-4 mt-lg-5">
-                    <div className="col-12 col-lg-3 px-lg-0 ms-auto">
-                        <select className="form-select-sm w-100 rounded-3 background-secondary color-quaternary mb-2" onChange={handleDiscussionsFilter} defaultValue={discussionsFilter}>
-                            <option value="1">All discussions</option>
-                            <option value="2">Followed users</option>
-                            <option value="3">My discussions</option>
-                        </select>
-                    </div>
-                </div>
+                    {discussions.map((item, index) => (
+                        <Discussion key={index} data={item} onDeleteDiscussion={handleDeleteDiscussion} />
+                    ))}
 
-                {discussions.map((item, index) => (
-                    <Discussion key={index} data={item} onDeleteDiscussion={handleDeleteDiscussion} />
-                ))}
+                    <Pagination currentPage={currentPage} lastPage={lastPage} totalDiscussions={totalDiscussions} onPageChange={handlePageChange} />
 
-                <Pagination currentPage={currentPage} lastPage={lastPage} totalDiscussions={totalDiscussions} onPageChange={handlePageChange} />
+                    <br />
+                </main>
+            }
 
-                <br />
-            </main>
             <LoadingOverlay loading={loading} />
         </div >
     )
